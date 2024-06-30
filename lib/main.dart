b30f4afa21a1ef7dcbe63a0b7bd2eeb4b87d80fa
Kwasi48 <abuwestwood@gmail.com>
2024-06-30 14:12:19 +0000

    adding products model

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,60 @@
-import 'package:boboshop/Views/checkout.dart';
-import 'package:boboshop/Views/products.dart';
+import 'package:boboshop/Views/checkoutView.dart';
+import 'package:boboshop/Views/productsView.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const BoboMain());
 }
 
- class BoboMain extends StatelessWidget {
-   const BoboMain({super.key});
+class BoboMain extends StatelessWidget {
+  const BoboMain({super.key});
 
-   @override
-   Widget build(BuildContext context) {
-     return   MaterialApp(
-       debugShowCheckedModeBanner: false,
-       theme: ThemeData(
-           colorScheme: ColorScheme.fromSeed( seedColor: Colors.greenAccent),
-       useMaterial3: true),
-       home: BoboHome(),
-     );
-   }
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.greenAccent),
+          useMaterial3: true),
+      home: BoboHome(),
+    );
+  }
+}
 
+class BoboHome extends StatefulWidget {
+  const BoboHome({super.key});
 
- class BoboHome extends StatefulWidget {
-   const BoboHome({super.key});
+  @override
+  State<BoboHome> createState() => _BoboHomeState();
+}
 
-   @override
-   State<BoboHome> createState() => _BoboHomeState();
- }
-
- class _BoboHomeState extends State<BoboHome> {
-
+class _BoboHomeState extends State<BoboHome> {
   int _selectedIndex = 0;
 
-  final _boboPages = [
-    ProductView(),
-    CheckoutView()
-  ];
-   @override
-   Widget build(BuildContext context) {
-     return  Scaffold(
-         appBar: AppBar(
-           backgroundColor: Theme.of(context).colorScheme.primary,
-           centerTitle: true,
-         title: const Text('BoboShop☕'),
-     ),
-     body: _boboPages[_selectedIndex] ,
-     bottomNavigationBar: BottomNavigationBar(
-         currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-       items: const [
-       BottomNavigationBarItem(icon: Icon(Icons.coffee), label: ''),
-         BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_checkout), label: '')]
+  final _boboPages = [ProductView(), CheckoutView()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        centerTitle: true,
+        title: const Text('BoboShop☕'),
+      ),
+      body: _boboPages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.coffee), label: ''),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.shopping_cart_checkout), label: '')
+          ]),
+    );
+  }
 
-     ),);
-   }
-   void _onItemTapped(int index){
-      setState(() {
-        _selectedIndex = index;
-      });
-   }
- }
-
-
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
---
+++ lib/models/products.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Product {
+  String name;
+  Image productImage;
+  double price;
+  String description;
+
+  Product(
+      {required this.name,
+      required this.productImage,
+      required this.price,
+      required this.description});
+}
